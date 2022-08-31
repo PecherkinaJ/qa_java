d3f99cc443a49651e7e27469c273b55226882aa8
Iuliia Bykadorova <ulashka-05@yandex.ru>
2022-08-31 19:17:44 +0300

    added new tests, coverage is 97%

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,81 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testGetKittens(){
+        Mockito.when(feline.getKittens()).thenReturn(5);
+        assertEquals(5, feline.getKittens());
+    }
+
+    @Test
+    public void testGetFamily(){
+        Mockito.when(feline.getFamily()).thenReturn("Куньи");
+        assertEquals("Куньи", feline.getFamily());
+    }
+
+    @Test
+    public void testEatMeatRealObjectShouldBeHerbivorousFood() throws Exception{
+        Feline realFeline = new Feline();
+        Feline spyFeline = Mockito.spy(realFeline);
+        Mockito.when(spyFeline.getFood("Травоядное")).thenReturn(List.of("Трава", "Различные растения"));
+        assertEquals(List.of("Трава", "Различные растения"), spyFeline.getFood("Травоядное"));
+    }
+
+    @Test
+    public void testEatMeatRealObjectShouldBePredatorFood() throws Exception{
+        Feline realFeline = new Feline();
+        Feline spyFeline = Mockito.spy(realFeline);
+        Mockito.when(spyFeline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), spyFeline.getFood("Хищник"));
+    }
+
+    @Test (expected = Exception.class)
+    public void testEatMeatRealObjectShouldBeException() throws Exception{
+        Feline realFeline = new Feline();
+        Feline spyFeline = Mockito.spy(realFeline);
+        try {
+            spyFeline.getFood("Животное");
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+
+    @Test
+    public void testGetKittensShouldBeOne(){
+        Feline curFeline = new Feline();
+        assertEquals(1, curFeline.getKittens());
+    }
+
+    @Test
+    public void testGetKittensShouldBeTwo(){
+        Feline curFeline = new Feline();
+        assertEquals(2, curFeline.getKittens(2));
+    }
+
+    @Test
+    public void testGetFamilyRealObject(){
+        Feline curFeline = new Feline();
+        assertEquals("Кошачьи", curFeline.getFamily());
+    }
+
+    @Test
+    public void testEatMeatRealObject() throws Exception{
+        Feline curFeline = new Feline();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), curFeline.eatMeat());
+    }
+
+}
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,5 +1,97 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+//@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
+    @Test
+    public void testLionConstructorCorrectMaleSex() throws Exception{
+        Lion lion = new Lion(new Feline(), "Самец");
+        assertTrue(lion.hasMane);
+    }
+    @Test
+    public void testLionConstructorCorrectFemaleSex() throws Exception{
+        Lion lion = new Lion(new Feline(), "Самка");
+        assertFalse(lion.hasMane);
+    }
+
+    @Test
+    public void testLionConstructorEmptySexShouldBeException() throws Exception {
+        try {
+            new Lion(new Feline(), "");
+        } catch (Exception e){
+            assertTrue(e.getMessage().contains("Используйте допустимые значения пола животного - самец или самка"));
+        }
+    }
+
+    @Test
+    public void testLionConstructorUndefineSexShouldBeException() throws Exception {
+        try {
+            new Lion(new Feline(), "Undefine");
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Используйте допустимые значения пола животного - самец или самка"));
+        }
+    }
+    @Test
+    public void testLionConstructorSymbolsInsteadOfSexShouldBeException() throws Exception {
+        try {
+            new Lion(new Feline(), "@#!*&()");
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Используйте допустимые значения пола животного - самец или самка"));
+        }
+    }
+
+    @Test
+    public void testDoesHaveManeTrue() throws Exception{
+        Lion lion = new Lion(new Feline(), "Самец");
+        assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void testDoesHaveManeFalse() throws Exception{
+        Lion lion = new Lion(new Feline(), "Самка");
+        assertFalse(lion.doesHaveMane());
+    }
+
+    @Test
+    public void testGetKittensMale() throws Exception{
+        Lion lion = new Lion(new Feline(), "Самец");
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void testGetKittensFemale()throws Exception{
+        Lion lion = new Lion(new Feline(), "Самка");
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void testEatMeatMale() throws Exception{
+        Feline feline = new Feline();
+        Lion lion = new Lion(feline, "Самец");
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+    }
+
+    @Test
+    public void testEatMeatFemale() throws Exception{
+        Feline feline = new Feline();
+        Lion lion = new Lion(feline, "Самка");
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+    }
+
+    @Test (expected = Exception.class)
+    public void testEatMeatFemaleException() throws Exception{
+        Feline feline = new Feline();
+        try {
+            new Lion(feline, "Животное");
+        } catch (Exception e){
+            throw new Exception(e);
+        }
+    }
 }
